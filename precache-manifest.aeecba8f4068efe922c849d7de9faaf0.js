974eaf9f93632ed40a6d64e4c2594b8ec5275b63
Pawel Multan <pj.multan@gmail.com>
2020-01-20 19:15:52 +0100

    Updates

---
+++ precache-manifest.aeecba8f4068efe922c849d7de9faaf0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1be9d4b4bee5e2f3155d88d8c0e3ce89",
+    "url": "/simple-reactjs-calculator/index.html"
+  },
+  {
+    "revision": "0fa03b17cfed04efe923",
+    "url": "/simple-reactjs-calculator/static/css/main.eac26cb2.chunk.css"
+  },
+  {
+    "revision": "8b4d468582a80dfacb52",
+    "url": "/simple-reactjs-calculator/static/js/2.006ff2e5.chunk.js"
+  },
+  {
+    "revision": "57a6ac8e3cf465b6fbd66e23fb4340ae",
+    "url": "/simple-reactjs-calculator/static/js/2.006ff2e5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0fa03b17cfed04efe923",
+    "url": "/simple-reactjs-calculator/static/js/main.90c36adc.chunk.js"
+  },
+  {
+    "revision": "6ce7df3f6af69832d562",
+    "url": "/simple-reactjs-calculator/static/js/runtime-main.59420e29.js"
+  },
+  {
+    "revision": "c13686c35b78b4abf231524f9c1101a3",
+    "url": "/simple-reactjs-calculator/static/media/bg.c13686c3.jpg"
+  }
+]);
